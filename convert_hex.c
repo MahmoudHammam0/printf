b9b85d0a19dfacd435d555ae6712da895aed4e8c
youssef <youssefelghamour@gmail.com>
2023-09-16 07:45:38 -0700

    add convert_hex.c

---
+++ convert_hex.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * dec_to_hex - convert a decimal to hexadecimal
+ * @n: decimal number
+ *
+ * Return: number in hexadecimal as a string
+ */
+char *dec_to_hex(int n)
+{
+	char dig[] = "0123456789abcdef";
+	char hex[1000];
+
+	int len = 0, i, j = 0, num = n;
+
+	while (num > 0)
+	{
+		int mod = num % 16;
+
+		hex[len] = dig[mod];
+		len++;
+		num = num / 16;
+	}
+
+	char *str = malloc(len + 1);
+
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+
+	for (i = len - 1; i >= 0; i--)
+		str[j++] = hex[i];
+
+	str[j] = '\0';
+
+	return (str);
+}
