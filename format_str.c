b32a3b5408a12681cc18d210b92589ec4424bac5
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-18 12:05:43 -0700

    format_str Update

--- format_str.c
+++ format_str.c
@@ -8,7 +8,7 @@
  */
 int format_str(const char *format, fmt spec[], va_list args)
 {
-	int j, n, sum = 0;
+	int n, sum = 0;
 	const char *f = format;
 
 	for ( ; *f; f++)
@@ -16,36 +16,51 @@ int format_str(const char *format, fmt spec[], va_list args)
 		if (*f == '%')
 		{
 			f++;
-			for (j = 0; spec[j].c != '\0'; j++)
-			{
-				if (*f == spec[j].c)
-				{
-					n = spec[j].f(args);
-					if (n == -1)
-						return (-1);
-					sum = sum + n;
-					break;
-				}
-			}
-			if (spec[j].c == '\0' && *f != ' ')
-			{
-				if (*f != '\0')
-				{
-					f--;
-					write(1, f, 1);
-					f++;
-					write(1, f, 1);
-					sum += 2;
-				}
-				else
-					return (-1);
-			}
+			n = format_str2(f, spec, args);
+			sum += n;
 		}
 		else
 		{
 			write(1, f, 1);
 			sum++;
 		}
+	}
+	return (sum);
+}
+/**
+ * format_str2 - print either normal characters or look for format specifiers
+ * @format: format string
+ * @spec: array of structures
+ * @args: arguments lists
+ * Return: number of characters printed
+ */
+int format_str2(const char *format, fmt spec[], va_list args)
+{
+	int i, m, sum = 0;
+
+	for (i = 0; spec[i].c != '\0'; i++)
+	{
+		if (*format == spec[i].c)
+		{
+			m = spec[i].f(args);
+			if (m == -1)
+				return (-1);
+			sum += m;
+			break;
+		}
+	}
+	if (spec[i].c == '\0' && *format != ' ')
+	{
+		if (*format != '\0')
+		{
+			format--;
+			write(1, format, 1);
+			format++;
+			write(1, format, 1);
+			sum += 2;
+		}
+		else
+			return (-1);
 	}
 	return (sum);
 }
